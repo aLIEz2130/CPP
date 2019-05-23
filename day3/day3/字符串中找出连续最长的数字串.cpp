aa7140d01f05940a8e1601614ed080c48a28cb07
aLIEz2130 <769101387@qq.com>
2019-05-23 20:42:14 +0800

    省去中间变量.

--- day3/day3/字符串中找出连续最长的数字串.cpp
+++ day3/day3/字符串中找出连续最长的数字串.cpp
@@ -8,7 +8,6 @@ int main()
 	getline(cin, s);
 	string tmp;
 	string maxstr;
-    int maxlength = 0;
 	for (size_t i = 0; i < s.size(); ++i)//遍历字符串
 	{
 		if (s[i] <= '9'&& s[i] >= '0')//找到数字串
@@ -18,10 +17,9 @@ int main()
 				tmp += s[i];
 				i++;
 			}
-			if (tmp.size()>maxlength)//新找到的数字串与之前找到的最长数字串比较
+			if (tmp.size()>maxstr.size())//新找到的数字串与之前找到的最长数字串比较
 			{
 				maxstr = tmp;
-				maxlength = tmp.size();
 			}
 		}
 		tmp.clear();//临时字符串每次用完清空
