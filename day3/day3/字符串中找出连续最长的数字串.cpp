ae7ed4cb9ff49b17f239de329b16db390a74051c
aLIEz2130 <769101387@qq.com>
2019-05-24 13:59:47 +0800

    取消while循环，对代码进行了优化。

--- day3/day3/字符串中找出连续最长的数字串.cpp
+++ day3/day3/字符串中找出连续最长的数字串.cpp
@@ -4,27 +4,64 @@
 using namespace std;
 int main()
 {
-	string s;	
-	getline(cin, s);
+	string s;
 	string tmp;
 	string maxstr;
-	for (size_t i = 0; i < s.size(); ++i)//遍历字符串
+	getline(cin, s);
+	for (size_t i = 0; i <= s.size(); ++i)
+	//for (size_t i = 0; i < s.size(); ++i) 会少进一次循环，将最后一个数字串存储在tmp但是没传递给maxstr，打印maxstr就只打印之前最长的数字串
+	//for (size_t i = 0; i <= s.length(); ++i)//string中size和length作用一样。。。
 	{
 		if (s[i] <= '9'&& s[i] >= '0')//找到数字串
 		{
-			while (s[i] <= '9'&& s[i] >= '0')//将找到的数字串所有元素存储到临时字符串tmp中
-			{
-				tmp += s[i];
-				i++;
-			}
+			tmp += s[i];
+		}
+		else 	{
 			if (tmp.size()>maxstr.size())//新找到的数字串与之前找到的最长数字串比较
 			{
 				maxstr = tmp;
 			}
+			else{
+				tmp.clear();//临时字符串每次用完清空
+			}
 		}
-		tmp.clear();//临时字符串每次用完清空
-	}
-	cout << maxstr<<endl;
+	 }
+
+	cout << maxstr << endl;
 	system("pause");
 	return 0;
 }
+
+
+
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	string s;	
+//	getline(cin, s);
+//	string tmp;
+//	string maxstr;
+//	for (size_t i = 0; i < s.size(); ++i)//遍历字符串
+//	{
+//		if (s[i] <= '9'&& s[i] >= '0')//找到数字串
+//		{
+//			while (s[i] <= '9'&& s[i] >= '0')//将找到的数字串所有元素存储到临时字符串tmp中
+//			{
+//				tmp += s[i];
+//				i++;
+//			}
+//			if (tmp.size()>maxstr.size())//新找到的数字串与之前找到的最长数字串比较
+//			{
+//				maxstr = tmp;
+//
+//			}
+//		}
+//		tmp.clear();//临时字符串每次用完清空
+//	}
+//	cout << maxstr<<endl;
+//	system("pause");
+//	return 0;
+//}
