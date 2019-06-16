5c33af8c4ff0cb25aa784446db22b3ace1a48bb0
aLIEz2130 <769101387@qq.com>
2019-06-16 22:33:01 +0800

    1

---
+++ day15---查找输入整数二进制中1的数/day15---查找输入整数二进制中1的数/test.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+int main()
+{ 
+	int n;
+	while (cin >> n)
+	{
+		int count = 0; 
+		while (n) 
+		{ 
+			if ((n & 1) == 1)
+				count += 1;
+			n >>= 1;
+		} 
+		cout << count << endl; 
+	}
+	return 0; 
+}
